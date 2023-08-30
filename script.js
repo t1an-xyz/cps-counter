83ec84d115d292c14bbeeaa48c99160057a68d53
t1an-xyz <62950302+t1an-xyz@users.noreply.github.com>
2023-08-29 21:20:45 -0400

    Add HTML, CSS, and Javascript
    Completed a basic version of the project that counts the number of
    clicks made within 5 seconds and displays the average clicks per second.

---
+++ script.js
@@ -0,0 +1,32 @@
+let active = false;
+let startTime = null;
+let myInterval = null;
+
+function timer() {
+    const timerLabel = document.getElementById('timer');
+    let time = +timerLabel.innerText + 0.1;
+    let clicks = +document.getElementById('counter').innerText;
+    let cps = clicks / time;
+    if (active) {
+        timerLabel.innerText = time.toFixed(1);
+        document.getElementById('cps').innerText = cps.toFixed(1);
+    }
+    if (timerLabel.innerText == "5.0") {
+        active = false;
+        alert('Clicks per second: ' + cps.toFixed(1));
+        clearInterval(myInterval);
+    }
+}
+
+function addClick() {
+    if (!startTime) {
+        startTime = Date.now();
+        active = true;
+        myInterval = setInterval(timer, 100);
+    }
+    if (active) {
+        const label = document.getElementById('counter');
+        let clicks = +label.innerText;
+        label.innerText = ++clicks;
+    }
+}
